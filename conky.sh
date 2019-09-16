39585570d31dd1dde9814301aaf6bb78ce78d063
Roman Lagutin <rlagutin@mta4.ru>
2019-09-16 13:29:07 +0300

    mod conky

---
+++ conky.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while [ -z "$(ps -eo args | grep -iw '^x11vnc' | grep -iv 'grep')" ]; do sleep 1; done
+sleep 10; fbsetroot -solid black; sleep 10; conky --config=/app/conky.conf 
+while [ ! -z "$(ps -eo args | grep -iw '^conky' | grep -iv 'grep')" ]; do sleep 1; done
